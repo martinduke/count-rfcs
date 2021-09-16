b81db2789dc75d58692308a2250ffc30fabba2cf
Martin Duke <martin.h.duke@gmail.com>
2021-09-16 13:05:48 -0700

    Added titles

--- count-rfcs.py
+++ count-rfcs.py
@@ -10,7 +10,7 @@ include_informational = False
 include_experimental = False
 include_acknowledgments = True
 first_year = 2013
-first_rfc = 4614
+first_rfc = 9130
 first_ad_year = 2020
 # The filters below only apply to RFCs published before 'first_ad_year'
 # Matching any of these is sufficient
@@ -21,14 +21,14 @@ areas = [ 'tsv' ]
 #all values: too many to list here; include 'NON' for non-WG
 wgs = [ ]
 
-# Result lists
-author = []
-responsible_ad = []
-shepherd = []
-contributor = []
-balloted = []
+# Result maps
+author = {}
+responsible_ad = {}
+shepherd = {}
+contributor = {}
+balloted = {}
 # not yet supported
-art_reviewer = []
+art_reviewer = {}
 
 rfced_url = 'https://www.rfc-editor.org/rfc-index2.html'
 rfced_req = urllib.request.Request(rfced_url)
@@ -60,6 +60,8 @@ for row in table.contents:
     if longline.find('Not Issued') >= 0:
         print ("Not issued")
         continue
+    title = row.td.find_next_sibling("td").b.get_text()
+    print('"' + title, end='": ')
 
     date = re.search(r"\[[ a-zA-Z0-9]+\]", longline)
     date = date[0].strip("[]")
@@ -92,9 +94,10 @@ for row in table.contents:
     if (not include_experimental) and (doc['Status'] == 'EXPERIMENTAL'):
         print("Discarding EXPERIMENTAL")
         continue
+
     if longline.find(name) >= 0:
         print("Authored")
-        authored.append(rfcnum.encode('ascii'))
+        author[rfcnum] = title
         continue
 
     # Extract the ballot
@@ -126,7 +129,7 @@ for row in table.contents:
     if (dt_html.find(uc_name) < 0):
         if ballot_review:
             print("Balloted")
-            balloted.append(rfcnum.encode('ascii'))
+            balloted[rfcnum] = title
         else:
             print("Name did not appear")
         continue
@@ -170,14 +173,14 @@ for row in table.contents:
             #print("AD: " + value)
             if value == full_name:
                 print("Responsible AD")
-                responsible_ad.append(rfcnum.encode('ascii'))
+                responsible_ad[rfc_num] = title
                 found = true
                 break
     if found:
         continue
 
     print("Acknowledged")
-    contributor.append(rfcnum.encode('ascii'))
+    contributor[rfcnum] = title
 
 
 
